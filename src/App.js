ac8731501dd3ceedb48391a86b9fc736e7566e85
whiteRa2bit <pavel.fakanov@gmail.com>
2020-08-02 20:42:41 +0300

    Add basic website

--- src/App.js
+++ src/App.js
@@ -1,25 +1,18 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import './styles/App.css';
+import HomePage from './components/HomePage';
+import { BrowserRouter, Route, Switch } from "react-router-dom";
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+      <div className="App">
+          <BrowserRouter basename='/'>
+             <Switch>
+                <Route exact path="/" component={HomePage} /> 
+              </Switch>  
+         </BrowserRouter>
+      </div>
   );
 }
 
---
+++ src/components/HomePage.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import '../styles/index.css';
+import { withRouter } from "react-router-dom";
+
+const HomePage = () => {
+    return (
+        <div>
+            <p> HomePage</p>
+        </div>
+    )
+}
+
+export default withRouter(HomePage)
